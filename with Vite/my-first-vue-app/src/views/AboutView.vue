79e47cd93d419953d9ebbd69e5a5fb983fa8641a
anushas-dev <72661377+anushas-dev@users.noreply.github.com>
2022-02-23 10:57:26 +0530

    added apps with and without Vite

---
+++ with Vite/my-first-vue-app/src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>ABOUT</h1>
+    <p>This is a static about page.</p>
+    <br />
+  </div>
+  <div class="left-content">
+    <h1>LEFT</h1>
+    <p>This is a left aligned component of the about section.</p>
+    <br />
+  </div>
+  <div class="right-content">
+    <h1>RIGHT</h1>
+    <p>This is a right aligned component of the about section.</p>
+    <br />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 50vh;
+    align-items: center;
+  }
+  .left-content {
+    align-items: left;
+  }
+  .right-content {
+    align-items: right;
+  }
+}
+</style>
